86f46f8af3e6abf0e9adcb7a19814dd1b93cabdc
apisit.hmc <apisit@peerpower.co.th>
2021-02-09 10:25:17 +0700

    [master] update repaymentSchedule fillable

--- app/RepaymentSchedule.php
+++ app/RepaymentSchedule.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class RepaymentSchedule extends Model
 {
     protected $fillable = [
-        'loan_amount', 'loan_term', 'interest_rate', 'start_date',
+        'id', 'payment_no', 'payment_date', 'principal', 'interest', 'balance', 'loan_id'
     ];
 
     public function loan()
